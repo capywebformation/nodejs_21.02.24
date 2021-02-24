37b9021ad66a0409661f6cdb3f3411425e4bfa20
atacanjacques <atacan.jacques@gmail.com>
2021-02-24 09:48:13 +0100

    Add routing

--- README.md
+++ README.md
@@ -8,7 +8,22 @@
 docker-compose up
 ```
 
+## Start docker
+```
+docker-compose start
+```
+
+## Remove container
+```
+docker-compose down
+```
+
+## Show container logs
+```
+docker-compose logs -f
+```
+
 ## Access to docker
 ```
-docker exec -ti container bash
+docker-compose exec node /bin/sh
 ```
--- src/app.js
+++ src/app.js
@@ -4,9 +4,29 @@ const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+  // let url = req.url;
+
+  let {url} = req;
+
+  switch (url) {
+    case "/":
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Home');
+      break;
+
+    case "/posts":
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end('Liste des articles');
+      break;
+  
+    default:
+      break;
+  }
+
+  
+
 });
 
 server.listen(port, hostname);
