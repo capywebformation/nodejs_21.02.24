9cad6567c7fce6992e39a042a530d808378002dd
atacanjacques <atacan.jacques@gmail.com>
2021-02-24 11:21:01 +0100

    Add mvc

--- src/app.js
+++ src/app.js
@@ -5,28 +5,14 @@ const port = 3000;
 
 const server = express();
 
-server.get("/", (req, res) => {
-  res.type('html');
-  res.status(200); // OK
-  res.end("Home");
-})
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinode');
 
-server.get("/posts", (req, res) => {
-  res.type('html');
-  res.status(200); // OK
-  res.end("Liste des articles");
-})
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
 
-server.get("/posts/:id_post", (req, res) => {
-  res.type('html');
-  res.status(200); // OK
-  res.end(`Àrticle : ${req.params.id_post}`);
-})
-
-server.post("/posts", (req, res) => {
-  res.type('html');
-  res.status(201); // Created
-  res.end("Article crée");
-})
+const postRoute = require('./routes/postRoute');
+postRoute(server);
 
 server.listen(port, hostname);
